e800d264d85ed768b34ed2c188967778e65a178e
Sachith Dunatunga <sdunatunga@alum.mit.edu>
2024-12-09 07:41:20 -0800

    update benchmark script

--- sims/benchmarkMDRmodel.sh
+++ sims/benchmarkMDRmodel.sh
@@ -4,20 +4,27 @@ set -euo pipefail
 # path to lmp executable
 LMP=../../../build/lmp
 
+cd test
+
 # non-adhesive simulations
 cd uniaxialCompression
+mkdir -p post
 ${LMP} < in.uniaxialCompression
 
 cd ../dieCompaction
+mkdir -p post
 ${LMP} < in.dieCompaction
 
 cd ../triaxialCompaction
+mkdir -p post
 ${LMP} < in.triaxialCompaction
 
 # adhesive simulations
 cd ../stickyParticleSandwich
+mkdir -p post
 ${LMP} < in.stickyParticleSandwich
 
 cd ../stickyUniaxialCompression
+mkdir -p post
 ${LMP} < in.stickyUniaxialCompression
 ${LMP} < in.plasticStickyUniaxialCompression

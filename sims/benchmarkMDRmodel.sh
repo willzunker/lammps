560e1499d1d1897d5820011834a5865b881fe2c7
Sachith Dunatunga <sdunatunga@alum.mit.edu>
2024-12-09 08:00:02 -0800

    use dump files instead of vtk output

--- sims/benchmarkMDRmodel.sh
+++ sims/benchmarkMDRmodel.sh
@@ -8,23 +8,18 @@ cd test
 
 # non-adhesive simulations
 cd uniaxialCompression
-mkdir -p post
 ${LMP} < in.uniaxialCompression
 
 cd ../dieCompaction
-mkdir -p post
 ${LMP} < in.dieCompaction
 
 cd ../triaxialCompaction
-mkdir -p post
 ${LMP} < in.triaxialCompaction
 
 # adhesive simulations
 cd ../stickyParticleSandwich
-mkdir -p post
 ${LMP} < in.stickyParticleSandwich
 
 cd ../stickyUniaxialCompression
-mkdir -p post
 ${LMP} < in.stickyUniaxialCompression
 ${LMP} < in.plasticStickyUniaxialCompression
